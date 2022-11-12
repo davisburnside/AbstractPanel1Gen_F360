5a29789df9e8fe4ea14f73b4fda3a6bd4d488af9
Davis Burnside <davis.burnside@gmail.com>
2022-11-12 13:06:27 -0500

    checkpoint

--- myAddonTest.py
+++ myAddonTest.py
@@ -135,96 +135,17 @@ class CreateExecuteHandler(adsk.core.CommandEventHandler):
         super().__init__()
     def notify(self, args):
         try:
+            eventArgs: adsk.fusion.CustomFeatureEventArgs = args
 
-            eventArgs = adsk.core.CommandEventArgs.cast(args)        
+            showMessage(f"hit 1")
 
-            # Get the settings from the inputs.
-            sketch: adsk.fusion.SketchPoint = _sketchSelectInput.selection(0).entity
-            # cuttingBody: adsk.fusion.SketchPoint = _cuttingBodySelectInput.selection(0).entity
-            
-            spawnEdge = _spawnBodyEdgeSelectInput.selection(0).entity
-            spawnBody = spawnEdge.body
+            directionLineMidpointsCol, copiedBodiesCol = spawnBodyCopies(args)
+            showMessage(f"hit 1.0")
+            showMessage(f"{directionLineMidpointsCol}, {copiedBodiesCol}")
+            showMessage(f"hit 1.1")
+            intersectBodiesWithSketchProfiles(args, directionLineMidpointsCol, copiedBodiesCol)
 
-            design: adsk.fusion.Design = _app.activeProduct
-            rootComp = design.rootComponent
-            spawnBodyComp = spawnBody.parentComponent
-            features = rootComp.features
-
-            firstFeature = None
-            lastFeature = None
-
-            sketchCurves = sketch.sketchCurves
-            lines = sketchCurves.sketchLines
-            index = -1
-            for line in lines:
-                if line.isConstruction:
-
-                    index += 1
-                    design: adsk.fusion.Design = _app.activeProduct
-                    rootComp = design.rootComponent
-                    spawnBodyComp = spawnBody.parentComponent
-
-                    # Get angle between points
-                    skPtStartData = line.startSketchPoint.geometry.getData()
-                    skPtEndData = line.endSketchPoint.geometry.getData()
-                    deltaX = -skPtStartData[1] + skPtEndData[1]
-                    deltaY = -skPtStartData[2] + skPtEndData[2]
-                    angle = math.degrees(math.atan2(deltaY, deltaX))
-
-                    # showMessage(f"{skPtStartData}, {skPtEndData}")
-                    # showMessage(f"{angle}, {deltaX}, {deltaY}")
-                    
-                    # Get world midpoint of sketch points
-                    skPtStartWorldData = line.startSketchPoint.worldGeometry.getData()
-                    skPtEndWorldData = line.endSketchPoint.worldGeometry.getData()
-                    midpoint_sketchPoint = adsk.core.Point3D.create(
-                        (skPtStartWorldData[1] + skPtEndWorldData[1]) / 2, 
-                        (skPtStartWorldData[2] + skPtEndWorldData[2]) / 2, 
-                        (skPtStartWorldData[3] + skPtEndWorldData[3]) / 2
-                        )
-
-                    # Get midpoint of body edge points
-                    spawnEdgeStartVert = spawnEdge.startVertex.geometry.getData()
-                    spawnEdgeEndVert = spawnEdge.endVertex.geometry.getData()
-                    midpoint_spawnEdge = adsk.core.Point3D.create(
-                        (spawnEdgeStartVert[1] + spawnEdgeEndVert[1]) / 2, 
-                        (spawnEdgeStartVert[2] + spawnEdgeEndVert[2]) / 2, 
-                        (spawnEdgeStartVert[3] + spawnEdgeEndVert[3]) / 2
-                        )
-
-                    # Create a matrix that defines the translation from point 1 to point 2.
-                    trans: adsk.core.Matrix3D = adsk.core.Matrix3D.create()
-                    rotationVector = vector = adsk.core.Vector3D.create(0, 1, 0)
-
-                    # Get Matrix for movement & rotation of body
-                    trans.setToRotation(math.radians(angle), rotationVector, midpoint_spawnEdge)
-                    newTransform = adsk.core.Matrix3D.create()
-                    newTransform.translation = midpoint_spawnEdge.vectorTo(midpoint_sketchPoint)
-                    trans.transformBy(newTransform)
-
-                    # Create Copy / Paste Feature
-                    newCopyFeature = spawnBodyComp.features.copyPasteBodies.add(spawnBody)
-                    baseBodyCopy = spawnBodyComp.bRepBodies.item(index + 1)
-                    baseBodyCopy.name = f"CopiedBody_{index}"
-                    if not firstFeature:
-                        firstFeature = newCopyFeature
-
-                    # Create Movement Feature
-                    moveFeatures = spawnBodyComp.features.moveFeatures
-                    inputEnts = adsk.core.ObjectCollection.create()
-                    inputEnts.add(baseBodyCopy)
-                    moveInput = moveFeatures.createInput(inputEnts, trans)
-                    moveFeature = moveFeatures.add(moveInput)
-                    lastFeature = moveFeature
-
-                    # newOcc = rootComp.occurrences.addExistingComponent (spawnBodyComp, trans)
-                    # newBody = newOcc.bRepBodies.item(0)
-
-            custFeatInput = spawnBodyComp.features.customFeatures.createInput(_customFeatureDef)
-            custFeatInput.addDependency('Sketch', sketch)
-            custFeatInput.addDependency('SpawnBodyEdge', spawnEdge)
-            custFeatInput.setStartAndEndFeatures(firstFeature, lastFeature)
-            spawnBodyComp.features.customFeatures.add(custFeatInput)
+            showMessage(f"hit 3")
 
         except:
             eventArgs.executeFailed = True
@@ -237,114 +158,9 @@ class ExecutePreviewHandler(adsk.core.CommandEventHandler):
         super().__init__()
     def notify(self, args):
         try:
-            eventArgs = adsk.core.CommandEventArgs.cast(args)        
-
-            # Get the settings from the inputs.
-            sketch: adsk.fusion.SketchPoint = _sketchSelectInput.selection(0).entity
-            # cuttingBody: adsk.fusion.SketchPoint = _cuttingBodySelectInput.selection(0).entity
             
-            spawnEdge = _spawnBodyEdgeSelectInput.selection(0).entity
-            spawnBody = spawnEdge.body
+            spawnBodyCopies(args)
 
-            design: adsk.fusion.Design = _app.activeProduct
-            rootComp = design.rootComponent
-            spawnBodyComp = spawnBody.parentComponent
-            features = spawnBodyComp.features
-
-            # baseFeat = features.baseFeatures.add()
-            # baseFeat.startEdit()
-            
-
-            sketchCurves = sketch.sketchCurves
-            lines = sketchCurves.sketchLines
-            for line in lines:
-                if line.isConstruction:
-
-                    skPtStartData = line.startSketchPoint.geometry.getData()
-                    skPtEndData = line.endSketchPoint.geometry.getData()
-
-                    deltaX = 1 #-skPtStartData[1] + skPtEndData[1]
-                    deltaY = 1 # -skPtStartData[2] + skPtEndData[2]
-                    angle = math.degrees(math.atan2(deltaY, deltaX))
-
-                    showMessage(f"{skPtStartData}, {angle}")
-
-                    design: adsk.fusion.Design = _app.activeProduct
-                    rootComp = design.rootComponent
-                    spawnBodyComp = spawnBody.parentComponent
-
-
-                    # Get the XYZ position of the point in model space.
-                    pointEnt = line.startSketchPoint
-                    pnt1: adsk.core.Point3D = None
-                    if pointEnt.objectType == adsk.fusion.SketchPoint.classType():
-                        skPoint: adsk.fusion.SketchPoint = pointEnt
-                        pnt1 = skPoint.worldGeometry
-                        # showMessage(f"is sketch geometry {pnt1}")
-                    else:
-                        pnt1 = pointEnt.geometry
-
-
-                    # pnt2 = adsk.core.Point3D.create(0, 0, 5)
-                    pnt2 = spawnEdge.endVertex.geometry
-
-                    # Create a matrix that defines the translation from point 1 to point 2.
-                    trans: adsk.core.Matrix3D = adsk.core.Matrix3D.create()
-                    rotationVector = vector = adsk.core.Vector3D.create(0, 1, 0)
-
-                    # trans.translation = pnt2.vectorTo(pnt1)
-                    # trans.translation = pnt2.vectorTo(pnt1)
-
-                    trans.setToRotation(angle, rotationVector, pnt2)
-                    
-                    newTransform = adsk.core.Matrix3D.create()
-                    newTransform.translation = pnt2.vectorTo(pnt1)
-                    trans.transformBy(newTransform)
-
-                    
-
-                    newOcc = rootComp.occurrences.addExistingComponent(spawnBodyComp, trans)
-                    newBody = newOcc.bRepBodies.item(0)
-                    
-            # baseFeat.finishEdit()
-                    
-                    # showMessage(f"{newOcc}, {newBody}, {trans2}")
-                    # bodyToRotate = adsk.core.ObjectCollection.create()
-                    # bodyToRotate.add(newOcc)
-                    # showMessage(f"{bodyToRotate}")
-
-                    # rotate the new body to align to sketch edge
-                    # trans2: adsk.core.Matrix3D = adsk.core.Matrix3D.create()
-                    # trans2.setToRotation(45, rotationVector, pnt1)
-                    # moveFeats = features.moveFeatures
-                    # moveFeatureInput = moveFeats.createInput(newBody, trans2)
-                    # moveFeats.add(moveFeatureInput)
-
-            # transform = adsk.core.Matrix3D.cast(rootComp.assemblyContext.transform)
-            # newTransform = adsk.core.Matrix3D.create()
-            # newTransform.translation = adsk.core.Vector3D.create(5.0, 10.0, 6.0)
-            # transform.transformBy(newTransform)
-
-
-
-            # pocketBody = CreatePocketBody(skPoint.worldGeometry, length, width, depth, radius)
-
-            # # Create a base feature and add the body.
-            # face = GetFaceUnderPoint(skPoint.worldGeometry)
-            # paramBody = face.body
-            # comp = paramBody.parentComponent
-
-            # baseFeat = comp.features.baseFeatures.add()
-            # baseFeat.startEdit()
-            # comp.bRepBodies.add(pocketBody, baseFeat)
-            # baseFeat.finishEdit()
-
-            # # Create a combine feature to subtract the pocket body from the part.
-            # toolBodies = adsk.core.ObjectCollection.create()
-            # toolBodies.add(baseFeat.bodies.item(0))
-            # combineInput = comp.features.combineFeatures.createInput(paramBody, toolBodies)
-            # combineInput.operation = adsk.fusion.FeatureOperations.CutFeatureOperation
-            # comp.features.combineFeatures.add(combineInput)
         except:
             showMessage('ExecutePreview: {}\n'.format(traceback.format_exc()))       
 
@@ -356,24 +172,130 @@ class ComputeCustomFeature(adsk.fusion.CustomFeatureEventHandler):
         try:
             eventArgs: adsk.fusion.CustomFeatureEventArgs = args
 
-            # Get the custom feature that is being computed.
-            custFeature = eventArgs.customFeature
+            # showMessage(f"hit 1b")
 
-            # Get the existing base feature and update the body.
-            baseFeature: adsk.fusion.BaseFeature = None
-            for feature in custFeature.features:
-                if feature.objectType == adsk.fusion.BaseFeature.classType():
-                    baseFeature = feature
-                    showMessage('\n Base Feature {baseFeature} \n')       
-                    break        
+            # directionLineMidpointsCol, copiedBodiesCol = spawnBodyCopies(args)
 
-            # # Update the body in the base feature.
-            # baseFeature.startEdit()
-            # body: adsk.fusion.BRepBody = baseFeature.bodies.item(0)
-            # baseFeature.updateBody(body, pocketBody)
-            # baseFeature.finishEdit()
+            # intersectBodiesWithSketchProfiles(args, directionLineMidpointsCol, copiedBodiesCol)
+
+            # showMessage(f"hit 3b")
+
         except:
             showMessage('CustomFeatureCompute: {}\n'.format(traceback.format_exc()))
+
+def spawnBodyCopies(args):
+
+    eventArgs = adsk.core.CommandEventArgs.cast(args)        
+
+    # Get the settings from the inputs.
+    sketch: adsk.fusion.SketchPoint = _sketchSelectInput.selection(0).entity
+    # cuttingBody: adsk.fusion.SketchPoint = _cuttingBodySelectInput.selection(0).entity
+    
+    spawnEdge = _spawnBodyEdgeSelectInput.selection(0).entity
+    spawnBody = spawnEdge.body
+
+    design: adsk.fusion.Design = _app.activeProduct
+    rootComp = design.rootComponent
+    spawnBodyComp = spawnBody.parentComponent
+    features = rootComp.features
+
+    firstFeature = None
+    lastFeature = None
+
+    # These two collections link to each other by index. 
+    # Points and Bodies are linked 1-1
+    directionLineMidpointsCol = adsk.core.ObjectCollection.create()
+    copiedBodiesCol = adsk.core.ObjectCollection.create()
+
+    sketchCurves = sketch.sketchCurves
+    lines = sketchCurves.sketchLines
+    index = -1
+    for line in lines:
+        if line.isConstruction:
+
+            index += 1
+
+            # Get angle between points
+            skPtStartData = line.startSketchPoint.geometry.getData()
+            skPtEndData = line.endSketchPoint.geometry.getData()
+            deltaX = -skPtStartData[1] + skPtEndData[1]
+            deltaY = -skPtStartData[2] + skPtEndData[2]
+            angle = math.degrees(math.atan2(deltaY, deltaX))
+
+            # showMessage(f"{skPtStartData}, {skPtEndData}")
+            # showMessage(f"{angle}, {deltaX}, {deltaY}")
+            
+            # Get world midpoint of sketch points
+            skPtStartWorldData = line.startSketchPoint.worldGeometry.getData()
+            skPtEndWorldData = line.endSketchPoint.worldGeometry.getData()
+            midpoint_sketchPoint = adsk.core.Point3D.create(
+                (skPtStartWorldData[1] + skPtEndWorldData[1]) / 2, 
+                (skPtStartWorldData[2] + skPtEndWorldData[2]) / 2, 
+                (skPtStartWorldData[3] + skPtEndWorldData[3]) / 2
+                )
+
+            # Get midpoint of body edge points
+            spawnEdgeStartVert = spawnEdge.startVertex.geometry.getData()
+            spawnEdgeEndVert = spawnEdge.endVertex.geometry.getData()
+            midpoint_spawnEdge = adsk.core.Point3D.create(
+                (spawnEdgeStartVert[1] + spawnEdgeEndVert[1]) / 2, 
+                (spawnEdgeStartVert[2] + spawnEdgeEndVert[2]) / 2, 
+                (spawnEdgeStartVert[3] + spawnEdgeEndVert[3]) / 2
+                )
+
+            # Create a matrix that defines the translation from point 1 to point 2.
+            trans: adsk.core.Matrix3D = adsk.core.Matrix3D.create()
+            rotationVector = vector = adsk.core.Vector3D.create(0, 1, 0)
+
+            # Get Matrix for movement & rotation of body
+            trans.setToRotation(math.radians(angle), rotationVector, midpoint_spawnEdge)
+            newTransform = adsk.core.Matrix3D.create()
+            newTransform.translation = midpoint_spawnEdge.vectorTo(midpoint_sketchPoint)
+            trans.transformBy(newTransform)
+
+            # Create Copy / Paste Feature
+            newCopyFeature = spawnBodyComp.features.copyPasteBodies.add(spawnBody)
+            baseBodyCopy = spawnBodyComp.bRepBodies.item(index + 1)
+            baseBodyCopy.name = f"CopiedBody_{index}"
+            if not firstFeature:
+                firstFeature = newCopyFeature
+
+            # Create Movement Feature
+            moveFeatures = spawnBodyComp.features.moveFeatures
+            inputEnts = adsk.core.ObjectCollection.create()
+            inputEnts.add(baseBodyCopy)
+            moveInput = moveFeatures.createInput(inputEnts, trans)
+            moveFeature = moveFeatures.add(moveInput)
+            lastFeature = moveFeature
+
+            directionLineMidpointsCol.add(midpoint_sketchPoint)
+            copiedBodiesCol.add(baseBodyCopy)
+
+            # newOcc = rootComp.occurrences.addExistingComponent (spawnBodyComp, trans)
+            # newBody = newOcc.bRepBodies.item(0)
+
+    custFeatInput = spawnBodyComp.features.customFeatures.createInput(_customFeatureDef)
+    custFeatInput.addDependency('Sketch', sketch)
+    custFeatInput.addDependency('SpawnBodyEdge', spawnEdge)
+    custFeatInput.setStartAndEndFeatures(firstFeature, lastFeature)
+    spawnBodyComp.features.customFeatures.add(custFeatInput)
+
+    return directionLineMidpointsCol, copiedBodiesCol
+
+def intersectBodiesWithSketchProfiles(args, directionLineMidpointsCol, copiedBodiesCol):
+
+    showMessage(f"hit 2")
+
+    showMessage(f"{directionLineMidpointsCol}, {copiedBodiesCol}")
+
+    # Things to consider:
+    #   I can't check if a point lies inside a profile, but I can check if it lies inside a bRedBody
+    #       BRepBody.pointContainment s
+
+    # Make a map associating each direction line midpoint with a new copied body
+    # Extrude all sketch profiles except boundary
+    # For each direction line midpoint, determine which new body it lies inside.
+    # Use that body to intersect with the copied body associated with direction line 
 
 def showMessage(message, error = False):
     textPalette: adsk.core.TextCommandPalette = _ui.palettes.itemById('TextCommands')
